fbcc3890299e4f0646bbb293228fb6daa46158d7
assaffmatias <matias.assaff@gmail.com>
2024-03-01 03:29:01 -0300

    responsive

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,6 @@
 import style from './About.module.css'
 import about from '../../assets/about.png'
+import aboutmob from '../../assets/about-mobile.png'
 
 const About = () => {
     return (
@@ -11,6 +12,7 @@ const About = () => {
                 </div>
                 <div className={style.aboutContent}>
                     <img src={about} alt="" className={style.about} />
+                    <img src={aboutmob} alt="" className={style.aboutmob} />
                 </div>
             </div>
         </div>
