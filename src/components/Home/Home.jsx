bbce551b50f5146062d9131fc6e9409fc528dcc5
assaffmatias <matias.assaff@gmail.com>
2024-03-02 01:04:58 -0300

    arrow

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,12 @@
 import style from './Home.module.css'
 
 const Home = () => {
+    const scrollDown = () => {
+        // Encuentra el elemento al que deseas desplazarte
+        const aboutSection = document.getElementById('about');
+        // Hace scroll hacia el elemento
+        aboutSection.scrollIntoView({ behavior: 'smooth' });
+    };
     return (
         <div className={style.container} id='home'>
             <div className={style.titleContent}>
@@ -10,6 +16,11 @@ const Home = () => {
                 <h1 className={style.title}>MATIAS ASSAFF</h1>
                 <div className={style.pContent}>
                     <p className={style.p}>i'm a Full Stack Developer</p>
+                </div>
+                <div className={style.arrow} onClick={scrollDown}>
+                    <span className={style.span}></span>
+                    <span className={style.span}></span>
+                    <span className={style.span}></span>
                 </div>
             </div>
         </div>
