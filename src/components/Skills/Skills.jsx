86c2b247288261b336a74bf32e6533d6726e5a6e
assaffmatias <matias.assaff@gmail.com>
2024-03-10 07:15:59 -0300

    update

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -10,67 +10,95 @@ import git from '../../assets/git.png'
 import node from '../../assets/node.png'
 import sequelize from '../../assets/sequelize.png'
 import postgres from '../../assets/postgres.png'
+import frontend from '../../assets/frontend.svg'
+import backend from '../../assets/backend.svg'
+import tools from '../../assets/tools.svg'
 import { useTranslation } from 'react-i18next';
-import i18n from '../../i18n';
 
 const Skills = () => {
-    const { i18n } = useTranslation();
     const { t } = useTranslation();
 
-    const changeLanguage = (lng) => {
-        i18n.changeLanguage(lng);
-    };
 
     return (
-        <div className={style.container}  id='skills'>
+        <div className={style.container} id='skills'>
             <div>
                 <h2 className={style.title}>{t('skills')}</h2>
                 <hr className={style.hr} />
             </div>
             <div className={style.mainContent}>
-                <div className={style.logoContent}>
-                    <img src={html} alt="" className={style.logo}/>
-                    <h3>HTML</h3>
+                <div className={style.skillContent}>
+                    <div className={style.spanContent}>
+                        <img src={frontend} alt="" className={style.svg} />
+                        <h5>
+                            <span className={style.spanFront}>Frontend</span>
+                        </h5>
+                    </div>
+                    <div className={style.imgContainer}>
+                        <div className={style.imgContent}>
+                            <img src={html} alt="" className={style.img} />
+                            <p className={style.p}>HTML</p>
+                        </div>
+                        <div className={style.imgContent}>
+                            <img src={css} alt="" className={style.img} />
+                            <p className={style.p}>CSS</p>
+                        </div>
+                        <div className={style.imgContent}>
+                            <img src={js} alt="" className={style.img} />
+                            <p className={style.p}>JavaScript</p>
+                        </div>
+                        <div className={style.imgContent}>
+                            <img src={ts} alt="" className={style.img} />
+                            <p className={style.p}>TypeScript</p>
+                        </div>
+                        <div className={style.imgContent}>
+                            <img src={react} alt="" className={style.img} />
+                            <p className={style.p}>React</p>
+                        </div>
+                        <div className={style.imgContent}>
+                            <img src={redux} alt="" className={style.img} />
+                            <p className={style.p}>Redux</p>
+                        </div>
+                        <div className={style.imgContent}>
+                            <img src={next} alt="" className={style.imgNext} />
+                            <p className={style.p}>Next JS</p>
+                        </div>
+                    </div>
                 </div>
-                <div className={style.logoContent}>
-                    <img src={css} alt="" className={style.logo}/>
-                    <h3>CSS</h3>
+                <div className={style.skillContent}>
+                    <div className={style.spanContent}>
+                        <img src={backend} alt="" className={style.svg} />
+                        <h5>
+                            <span className={style.spanBack}>Backend</span>
+                        </h5>
+                    </div>
+                    <div className={style.imgContainer}>
+                        <div className={style.imgContent}>
+                            <img src={node} alt="" className={style.img} />
+                            <p className={style.p}>Node JS</p>
+                        </div>
+                        <div className={style.imgContent}>
+                            <img src={sequelize} alt="" className={style.img} />
+                            <p className={style.p}>Sequelize</p>
+                        </div>
+                        <div className={style.imgContent}>
+                            <img src={postgres} alt="" className={style.img} />
+                            <p className={style.p}>PostgreSQL</p>
+                        </div>
+                    </div>
                 </div>
-                <div className={style.logoContent}>
-                    <img src={js} alt="" className={style.logo}/>
-                    <h3>JavaScript</h3>
-                </div>
-                <div className={style.logoContent}>
-                    <img src={ts} alt="" className={style.logo}/>
-                    <h3>TypeScript</h3>
-                </div>
-                <div className={style.logoContent}>
-                    <img src={react} alt="" className={style.logo}/>
-                    <h3>React</h3>
-                </div>
-                <div className={style.logoContent}>
-                    <img src={redux} alt="" className={style.logo}/>
-                    <h3>Redux</h3>
-                </div>
-                <div className={style.logoContent}>
-                    <img src={next} alt="" className={style.logo}/>
-                    <h3>Next JS</h3>
-                </div>
-                <div className={style.logoContent}>
-                    <img src={git} alt="" className={style.logo}/>
-                    <h3>GIT</h3>
-                </div>
-                <div className={style.logoContent}>
-                    <img src={node} alt="" className={style.logo}/>
-                    <h3>Node JS</h3>
-                </div>
-                <div className={style.logoContent}>
-                    <img src={sequelize} alt="" className={style.logo}/>
-                    <h3>Sequelize</h3>
-                </div>
-                <div className={style.logoContent}>
-                    <img src={postgres} alt="" className={style.logo}/>
-                    <h3>PostgreSQL</h3>
+                <div className={style.skillContent}>
+                    <div className={style.spanContent}>
+                        <img src={tools} alt="" className={style.svg} />
+                        <h5>
+                            <span className={style.spanTools}>{t('skillSpan')}</span>
+                        </h5>
+                    </div>
+                    <div className={style.imgContainer}>
+                        <div className={style.imgContent}>
+                            <img src={git} alt="" className={style.img} />
+                            <p className={style.p}>GIT</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- src/i18n.js
+++ src/i18n.js
@@ -20,7 +20,10 @@ i18n
 
           //About
           "aboutTitle": "Nice to meet you",
-          "aboutP": "I am a passionate web developer with a user-centered focus and a meticulous eye for design. I strive to turn ideas into captivating and functional web new every day, work on projects, and creatively solve problems. I enjoy sharing my knowledge with others and learning from their experiences. I am excited to continue building impactful web experiences and am available to discuss how I can hel take you next project to the next level!",
+          "aboutP": "I'm a passionate web developer with a user-centered focus and a meticulous eye for design. I strive to turn ideas into captivating and functional web. I love to investigate, solve problems, learn from others and teach, that is why in me you will find a developer with conflict resolution skills, willing to listen to ideas and contribute all my knowledge to achieve the proposed objectives. I'm excited to continue building impactful web experiences!",
+
+          //Skills
+          "skillSpan": "Tools",
 
          //Works
          "development": "Web Development",
@@ -29,7 +32,7 @@ i18n
          //Contact
          "contactName": "Name",
          "contactEmail": "Email",
-         "contactMessage": "Message...",
+         "contactMessage": "Your Message",
          "contactSubmit": "Submit",
          "builtBy": "Built by Matias Assaff",
         }
@@ -49,7 +52,10 @@ i18n
 
           //About
           "aboutTitle": "Encantado en conocerte",
-          "aboutP": "Soy un desarrollador web con un enfoque centrado en el usuario y un ojo meticuloso para el diseño, me esfuerzo por convertir las ideas en experiencias web cautivadoras y funcionales. Mi gusto por el aprendizaje continuo me impulsa a aprender algo nuevo todos los días, trabajar en proyector y resolver problemas creativamente. Disfruto compartir mis conocimientos con los demás y aprender de sus experiencias. Estoy emocionado de seguir construyendo experiencias web impactantes y estoy disponible para discutir como puedo ayudar a llevar tu próximo proyecto al siguiente nivel!",
+          "aboutP": "Soy un desarrollador web con un enfoque centrado en el usuario y un ojo meticuloso para el diseño, me esfuerzo por convertir las ideas en experiencias web cautivadoras y funcionales. Me encanta investigar, resolver problemas, aprender de los demás y enseñar, por eso en mí Encontrarás un desarrollador con capacidad de resolución de conflictos, dispuesto a escuchar ideas y aportar todos mis conocimientos para lograr los objetivos propuestos. Estoy emocionado de seguir construyendo experiencias web impactantes!",
+
+          //Skills
+          "skillSpan": "Herramientas",
 
           //Works
           "development": "Desarrollo Web",
@@ -58,7 +64,7 @@ i18n
           //Contact
          "contactName": "Nombre",
          "contactEmail": "Correo Electrónico",
-         "contactMessage": "Mensaje...",
+         "contactMessage": "Tu mensaje",
          "contactSubmit": "Enviar",
          "builtBy": "Desarrollado por Matias Assaff",
         }
