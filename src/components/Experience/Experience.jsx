568a95e1742348e4c00e6ef6585652eee100445d
Matias <matias.assaff98@gmail.com>
2024-07-30 06:35:13 -0300

    update

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -68,7 +68,7 @@ const Experience = () => {
                                         <path fill-rule="evenodd" d="M11.403 5H5a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-6.403a3.01 3.01 0 0 1-1.743-1.612l-3.025 3.025A3 3 0 1 1 9.99 9.768l3.025-3.025A3.01 3.01 0 0 1 11.403 5Z" clipRule="evenodd" />
                                         <path fillRule="evenodd" d="M13.232 4a1 1 0 0 1 1-1H20a1 1 0 0 1 1 1v5.768a1 1 0 1 1-2 0V6.414l-6.182 6.182a1 1 0 0 1-1.414-1.414L17.586 5h-3.354a1 1 0 0 1-1-1Z" clipRule="evenodd" />
                                     </svg>
-                                    <Link className={styles.link} to={'https://www.linkedin.com/company/softwind-solutions/'} target='_blank'>
+                                    <Link className={styles.link} to={'https://softwind.com.ar/'} target='_blank'>
                                         Softwind.com.ar
                                     </Link>
                                 </div>
